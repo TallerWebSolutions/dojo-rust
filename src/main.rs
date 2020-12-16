b5dcbc44e1e1c2b0a30f905e00ed9601e41bb111
marcoslehmkuhl <m.v.lehmkuhl@gmail.com>
2020-12-16 12:26:51 -0300

    dojo: mumbling done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::iter;
+
+fn accum(text: &str) -> String {
+    let list: Vec<String> = text
+        .chars()
+        .enumerate()
+        .map(|(index,letter)| { 
+            let repeated = iter::repeat(letter.to_lowercase().to_string())
+                .take(index)
+                .collect::<String>();
+             
+            return format!("{}{}", letter.to_uppercase(), repeated);
+            
+        })  
+        .collect::<Vec<String>>();
+
+    return list.join("-");
+}
+
+#[test]
+fn test() {
+    assert_eq!(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+    assert_eq!(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+    assert_eq!(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+    assert_eq!(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+    assert_eq!(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+}
+
 fn main() {
-    println!("Hello, world!");
+    //accum("a");
 }
